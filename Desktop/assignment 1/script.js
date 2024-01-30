4ac2ca4bc4b1a0a2a8e4d167924115bd8a4fb2cf
pavankumar2724 <maddinenipavankumar2000@gmail.com>
2024-01-29 18:40:11 -0600

    file added

--- Desktop/assignment 1/script.js
+++ Desktop/assignment 1/script.js
@@ -1,6 +1,31 @@
+var tableData = [
+    { name: 'pavan', state: 'kansas', age: 23 },
+    { name: 'vamsi', state: 'arkansas', age: 26 },
+    { name: 'harsha', state: 'texas', age: 35 },
+    { name: 'saradhi', state: 'arkansas', age: 45 },
+    { name: 'ajay', state: 'texas', age: 32 }
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+    populateTable(tableData);
+});
+
+function populateTable(data) {
+    var table = document.querySelector('table');
+    var tableBody = table.createTBody();
+
+    data.forEach(function (item) {
+        var row = tableBody.insertRow();
+        for (var key in item) {
+            var cell = row.insertCell();
+            cell.textContent = item[key];
+        }
+    });
+}
+
 function searchTable() {
     var searchValue = document.getElementById('searchInput').value.toLowerCase();
-    var tableRows = document.querySelectorAll('table tr:not(:first-child)');
+    var tableRows = document.querySelectorAll('table tbody tr');
 
     tableRows.forEach(function (row) {
         var showRow = false;
